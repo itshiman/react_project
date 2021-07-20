2c63a5bb774315a12b2383ee7650a3601fa2f2d0
Himanshu <himanshu092017@gmail.com>
2021-07-20 20:43:34 +0530

    Contact Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,9 @@ import { DISHES } from '../shared/dishes';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import Contact from './ContactComponent';
 
 
 class Main extends Component {
@@ -12,22 +15,26 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
-            selectedDish:null
         };
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId});
-    }
-
     render() {
+
+        const HomePage= () => {
+            return(
+                <Home/>
+            );
+        }
+
         return (
             <div>
                 <Header/>
-                <Menu dishes={this.state.dishes}
-                    onClick={(dishId) => this.onDishSelect(dishId)}/>
-                <DishDetail 
-                    dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]}/>
+                <Switch>
+                    <Route path="/home" component ={HomePage}/>
+                    <Route exact path="/menu" component ={() => <Menu dishes={this.state.dishes}/>} />
+                    <Route exact path ="/contactus" component= {Contact}/>
+                    <Redirect to ="/home"/>
+                </Switch>
                 <Footer/>
             </div>
         );
